b94fe7f321addf748df7a66a339cec08252d7406
刘循光 <liuxg@channelsoft.com>
2018-11-07 14:59:56 +0800

    修改代码

--- src/main/java/com/example/demo/service/MyService.java
+++ src/main/java/com/example/demo/service/MyService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.po.MyUser;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.access.prepost.PreFilter;
@@ -16,6 +17,8 @@ public class MyService {
 
     @PreAuthorize("hasAnyAuthority('server','admin') && #myUsers.size() > 0")
     @PreFilter("hasPermission(#myUsers,'xxdddd')")
+    @PostFilter("")
+    @PostAuthorize("")
     public List<MyUser> queryUser(List<MyUser> myUsers) {
         System.out.println(myUsers.size());
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
